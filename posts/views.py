e37c96649d3467f30665361afb8028cbe77ea130
milon19 <milonmahato67@gmail.com>
2020-11-10 02:54:16 +0600

    [ADDED] ADD JSON PLACEHOLDER FOR GET COMMENTS
    Integrate JSON PlaceHolder for get comments data
        - Get Comments from JSON Placeholder.
        - Modify Post Details Views for get comments.
    
    Resolves: #3

--- posts/views.py
+++ posts/views.py
@@ -22,10 +22,15 @@ class PostDetailView(TemplateView):
     def get(self, request, *args, **kwargs):
         post_id = kwargs['post_id']
         post_url = 'http://jsonplaceholder.typicode.com/posts/{}'.format(post_id)
+        comment_url = 'http://jsonplaceholder.typicode.com/posts/{}/comments'.format(post_id)
         req_post = requests.get(post_url)
+        req_comment = requests.get(comment_url)
+
         post = req_post.json()
+        comments = req_comment.json()
 
         context = {
             "post": post,
+            "comments": comments,
         }
         return render(request, 'blog-post.html', context)
