1c7cafa420a51c9b3acefd2d2e67cf8c8acc9c08
milon19 <milonmahato67@gmail.com>
2020-11-09 17:26:17 +0600

    [CREATE] LOGOUT FEATURE IMPLEMENTATION
    Implement user logout.
        - User can logout with navbar button.
        - Create a logout page.
    
    Resolves: #1

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from authentication.views import UserLoginView, register
+from authentication.views import UserLoginView, UserLogoutView, register
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name="login"),
+    path('logout/', UserLogoutView.as_view(), name="logout"),
     path('register/', register),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404
 from django.shortcuts import render
@@ -6,11 +6,15 @@ from authentication.forms import UserLoginForm
 
 
 # LOGIN VIEW ENDPOINT
-
 class UserLoginView(SuccessMessageMixin, LoginView):
     redirect_authenticated_user = True
     template_name = 'login.html'
     authentication_form = UserLoginForm
+
+
+# LOGOUT VIEW ENDPOINT
+class UserLogoutView(LogoutView):
+    template_name = 'logout.html'
 
 
 def register(request):
