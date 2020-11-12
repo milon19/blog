e7454029c88c689209821387c7c7ef7ff8abc8be
milon19 <milonmahato67@gmail.com>
2020-11-13 00:59:42 +0600

    [ADDED/FIXED] FIXED SOME ISSUES AND ADDED SOME FEATURE
    Fixing some issues and Adding some features
        - Clean up code for both React and Django
        - NavBar link changes issues fixed in React
        - Admin User Creation feature added in Django
        - Add beautiful toast message on Django
    
    Resolves: #10

--- react_frontend/src/app/components/LogoutComponent/LogoutComponent.js
+++ react_frontend/src/app/components/LogoutComponent/LogoutComponent.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Logout = () => {
-  const handleLogout = () => {
-    localStorage.setItem("accessToken", null);
-  };
+  // window.location.href = "/logout";
   return (
     <div
       className="container"
@@ -13,7 +11,7 @@ const Logout = () => {
       <h2>You have been logged out</h2>
       <div className="border-top pt-3">
         <p>
-          <Link className="btn btn-danger" onClick={handleLogout()} to="/login">
+          <Link className="btn btn-danger" to="/login">
             Log in again
           </Link>
         </p>
