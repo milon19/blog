0b3213a8d82e170b4fed7a05f533c29004068a24
milon19 <milonmahato67@gmail.com>
2020-11-13 17:05:15 +0600

    [MODIFY] FINALIZE FOR DEPLOY
    Ready to Deploy

--- react_frontend/views.py
+++ react_frontend/views.py
@@ -5,7 +5,7 @@ from grayspaceit import settings
 from django.http import HttpResponse
 
 
-index_file_path = os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')
+index_file_path = os.path.join(settings.REACT_APP_DIR, 'serve_from_django', 'index.html')
 
 
 def index(request):
